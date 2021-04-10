e171b981a40c2c43356174da81945c5f0be487df
liubowolkova <10179875+liubowolkova@users.noreply.github.com>
2021-04-10 09:57:24 +0300

    Task 1 is rewritten. All constraints were set by hand

--- Module_9/Task1_3Controller.swift
+++ Module_9/Task1_3Controller.swift
@@ -8,18 +8,8 @@
 import UIKit
 
 class Task1_3Controller: UIViewController {
-    @IBOutlet weak var button1HeigthConstraint: NSLayoutConstraint!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        let screenSize = UIScreen.main.bounds
-        let halfOfScreenSize = screenSize.height / 2
-        let oneButtonHeight = halfOfScreenSize / 3
-        self.changeConstraint(self.button1HeigthConstraint, oneButtonHeight)
-    }
-    
-    func changeConstraint(_ constraint: NSLayoutConstraint, _ value: CGFloat) {
-        constraint.constant = value
     }
 }
--- Module_9/Task1_4Controller.swift
+++ Module_9/Task1_4Controller.swift
@@ -8,17 +8,9 @@
 import UIKit
 
 class Task1_4Controller: UIViewController {
-    @IBOutlet weak var stackViewHeightConstraint: NSLayoutConstraint!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        let screenSize = UIScreen.main.bounds
-        let halfOfScreenSize = screenSize.height / 2
-        self.changeConstraint(self.stackViewHeightConstraint, halfOfScreenSize)
     }
-    
-    func changeConstraint(_ constraint: NSLayoutConstraint, _ value: CGFloat) {
-        constraint.constant = value
-    }
-}
+ }
